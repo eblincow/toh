42171ba5ffe03a1483ece16cb00edbf24ba7771a
Eric Blincow <eb1@fastmail.fm>
2015-02-22 22:16:38 -0500

    AJJAJ

--- game.py
+++ game.py
@@ -1,4 +1,28 @@
-from get_move import check_overlap, ALPHA_NUM, winning_combinations
+
+# Numbers to Alphabet and Alphabet to Numbers
+NUM_ALPHA = {'1':'a','2':'b','3':'c','4':'d','5':'e','6':'f','7':'g','8':'h','9':'i'}
+ALPHA_NUM = dict(zip(NUM_ALPHA.values(),NUM_ALPHA.keys()))
+
+
+winning_combinations = [
+        [1,2,3],[4,5,6],[7,8,9],
+        [1,4,7],[2,5,8],[3,6,9],
+        [1,5,9],[3,5,7]
+        ]
+
+
+
+
+
+def decode_move(some_string):
+    if some_string.isdigit() and 10 > int(some_string) > 0:
+        alpha = NUM_ALPHA.get(some_string)
+        return {alpha:'X'}
+    else:
+        return {}
+
+
+
 
 class Game():
     def __init__(self):
@@ -36,9 +60,18 @@ class Game():
                 self.Os.append(ALPHA_NUM[key])
         return self.Os
 
+   
 
 
 
+
+
+def decode_move(some_string):
+    if some_string.isdigit() and 10 > int(some_string) > 0:
+        alpha = NUM_ALPHA.get(some_string)
+        return {alpha:'X'}
+    else:
+        return {}
 
 
 
